7f7ab9ba31572e04e5abd4a9a2dd4ebed0198cdf
rarredondo9 <rebeca.arredondo.9@gmail.com>
2025-07-15 10:30:25 -0700

    week2

---
+++ week02/scripts/bom.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function() {
+  if (input.value.trim() !== '') {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+
+    deleteButton.classList.add('delete');
+
+    li.appendChild(deleteButton);
+
+    list.appendChild(li);
+
+    deleteButton.addEventListener('click', function () {
+      list.removeChild(li);
+      input.focus();
+    });
+
+    input.value = '';
+  }
+
+  input.focus();
+});
